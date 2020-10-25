8720d2548f3834508d2176afa8af34d70a485bcf
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-25 16:50:01 -0400

    updating context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
+import Login from "./context/Login";
 import MoviePage from "./context/MoviePage";
 import UserContext from "./context/userContext";
 
 export default class App extends Component {
-  state = { currentUser: { name: "Tucker" } };
+  handleLoggedIn = (username) => {
+    console.log(username);
+    const user = { name: "Dave" };
+    this.setState({ currentUser: user });
+  };
+
+  state = { currentUser: null };
 
   render() {
     return (
-      <UserContext.Provider value={this.state.currentUser}>
+      <UserContext.Provider
+        value={{
+          currentUser: this.state.currentUser,
+          onLoggedIn: this.handleLoggedIn,
+        }}
+      >
         <div>
           <MoviePage />
+          <Login />
         </div>
       </UserContext.Provider>
     );
---
+++ src/context/MovieRow.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import UserContext from "./userContext";
+
+function MovieRow(props) {
+  const userContext = useContext(UserContext);
+  return (
+    <div>
+      Movie Row {userContext.currentUser ? userContext.currentUser.name : "!"}
+    </div>
+  );
+}
+
+export default MovieRow;
